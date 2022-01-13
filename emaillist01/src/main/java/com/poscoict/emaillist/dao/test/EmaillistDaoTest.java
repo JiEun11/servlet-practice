5969fc9695371d00836d7fa8d36254bce08a4e80
Bella <shdudtnr3939@naver.com>
2022-01-13 09:47:59 +0900

    22.01.13 DAO insert test case 추

--- emaillist01/src/main/java/com/poscoict/emaillist/dao/test/EmaillistDaoTest.java
+++ emaillist01/src/main/java/com/poscoict/emaillist/dao/test/EmaillistDaoTest.java
@@ -8,13 +8,27 @@ import com.poscoict.emaillist.vo.EmaillistVo;
 public class EmaillistDaoTest {
 
 	public static void main(String[] args) {
-		testFindAll();
+		testInsert();
+//		testFindAll();
 	}
 
+	private static void testInsert() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("김");
+		vo.setLastName("지은");
+		vo.setEmail("32161108@dankook.ac.kr");
+		
+		boolean result = new EmaillistDao().insert(vo);
+		System.out.println(result? "success" : "fail");
+		
+	}
+	
 	private static void testFindAll() {
 		List<EmaillistVo> list = new EmaillistDao().findAll();
 		for(EmaillistVo vo : list) {
 			System.out.println(vo);
 		}
+		
+//		assertEqual(2, list.size()); 
 	}
 }
